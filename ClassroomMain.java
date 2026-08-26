92f258dfb74272ef9cc35b1932178324c82ba670
at1680 <aidantt2009@gmail.com>
2026-08-26 19:39:05 +0000

    Classroom Changes

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,5 +1,6 @@
 public class ClassroomMain {
     public static void main(String[] args) {
-        
+        Classroom morris = new Classroom("Morris");
+        Classroom Becker = new Classroom("Becker", 35, 32);
     }
 }
