021876fdccfc5f1d88f60a743eee784b8af52fac
at1680 <aidantt2009@gmail.com>
2026-08-27 03:41:10 +0000

    Classroom Main Changse

--- ClassroomMain.java
+++ ClassroomMain.java
@@ -1,6 +1,10 @@
 public class ClassroomMain {
     public static void main(String[] args) {
         Classroom morris = new Classroom("Morris");
-        Classroom Becker = new Classroom("Becker", 35, 32);
+        Classroom becker = new Classroom("Becker", 35, 32);
+
+        morris.setDesks(30);
+
+        System.out.println("The teacher is, " + becker.teacher() + "; and there are this many desks, " + becker.desks() + "; and this many students, " + becker.students());
     }
 }
